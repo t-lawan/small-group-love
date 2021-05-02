665700fe5d6e7d994fb8bfe3dbc0e20d8c7b4213
akinsola <thomas.lawanson@gmail.com>
2021-05-02 19:10:46 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Ge Hinnom`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
+        include_favicon: false,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
