68ba3a5b77c230c6ad297466851e5139b992983d
akinsola <thomas.lawanson@gmail.com>
2021-09-06 21:07:01 +0100

    udpat

--- src/components/project/upcoming-projects.js
+++ src/components/project/upcoming-projects.js
@@ -1,31 +1,40 @@
 import React from "react"
 import styled from "styled-components"
 import { connect } from "react-redux"
-import moment from 'moment';
-import { ProjectsWrapper, ProjectContentWrapper, ProjectPageTitle } from "./current-projects";
-import ProjectList from "./project-list";
-import { ProjectTypes } from "../../utility/helper";
-import ProjectContent from "../project-content/project-content";
+import moment from "moment"
+import {
+  ProjectsWrapper,
+  ProjectContentWrapper,
+  ProjectPageTitle
+} from "./current-projects"
+import ProjectList from "./project-list"
+import { ProjectTypes } from "../../utility/helper"
+import ProjectContent from "../project-content/project-content"
 const UpcomingProjects = props => {
   // let projects = props.projects.filter(pr => {
   //   return moment().isBefore(pr.start_date, 'day');
   // })
 
   let projects = props.projects.filter(pr => {
-    return pr.pages.includes(ProjectTypes.UPCOMING);
+    return pr.pages ?  pr.pages.includes(ProjectTypes.UPCOMING): false;
   })
-
-  projects.sort((a, b) => {
-      return moment(a).diff(moment(b));
-  })
+  if (projects) {
+    projects.sort((a, b) => {
+      return moment(a).diff(moment(b))
+    })
+  }
 
   return (
     <ProjectsWrapper>
       <ProjectPageTitle> Forthcoming Projects</ProjectPageTitle>
-      <ProjectList projects={projects} />
-      <ProjectContentWrapper>
-        <ProjectContent project={projects[0]} hideTitle={true} />
-      </ProjectContentWrapper>
+      {projects ? (
+        <>
+          <ProjectList projects={projects} />
+          <ProjectContentWrapper>
+            <ProjectContent project={projects[0]} hideTitle={true} />
+          </ProjectContentWrapper>
+        </>
+      ) : null}
     </ProjectsWrapper>
   )
 }
